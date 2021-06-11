353038a7b9ae9cf34e344857b8747713196086c0
vinicius-rosa <viniciusrosarafaeldeoliveira@gmail.com>
2021-06-11 18:32:52 -0300

    add head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,14 @@
+import Head from 'next/head';
 import { Layout } from '../src/components'
 import '../styles/globals.css'
 import "antd/dist/antd.css";
 
 function MyApp({ Component, pageProps }) {
   return <Layout>
+    <Head>
+      <title>Vitrinni Carioca</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
     <Component {...pageProps} />
   </Layout>
 }
